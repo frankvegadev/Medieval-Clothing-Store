11da8536bdaa11ea4d4aa3a323263f752638fadc
Franco Vega <frankvegadev@gmail.com>
2024-02-05 23:48:28 -0300

    refact: clean up game controller

--- MedievalClothingStore/Assets/Game/Scripts/GameController.cs
+++ MedievalClothingStore/Assets/Game/Scripts/GameController.cs
@@ -1,10 +1,7 @@
 using UnityEngine;
 
 using Common.Player;
-using Common.GameItems.Config;
-using Common.GameItems.Instance;
 using Common.Purchases;
-using Common.Purchases.Config;
 
 using Game.Constants;
 using Game.Store;
@@ -16,13 +13,10 @@ namespace Game
         #region EXPOSED_FIELDS
         [SerializeField] private PlayerController playerController = null;
         [SerializeField] private PurchaseController purchaseController = null;
+        [SerializeField] private int startingCoins = 0;
 
         [Header("Scene Config")]
         [SerializeField] private StoreController storeController = null;
-
-        [Header("Test")]
-        [SerializeField] private GameItemConfig[] testItemConfigs = null;
-        [SerializeField] private BuyableItemConfig[] buyTestConfigs = null;
         #endregion
 
         #region UNITY_CALLS
@@ -43,39 +37,7 @@ namespace Game
             playerController.ConfigureInput(InputConstants.movementAxisYInput, InputConstants.movementAxisXInput, InputConstants.toggleInventoryInput,
                 InputConstants.interactInput);
 
-            GiftItemsToPlayer();
-
-            playerController.AddCoins(550);
-
-            TrySellGiftedItems();
-
-            TryBuyItems();
-        }
-
-        private void GiftItemsToPlayer()
-        {
-            for (int i = 0; i < testItemConfigs.Length; i++)
-            {
-                playerController.TryAddItemToInventory(testItemConfigs[i]);
-            }
-        }
-
-        private void TryBuyItems()
-        {
-            for (int i = 0; i < buyTestConfigs.Length; i++)
-            {
-                purchaseController.TryBuyItem(buyTestConfigs[i]);
-            }
-        }
-
-        private void TrySellGiftedItems()
-        {
-            GameItemInstanceModel[] validItems = playerController.GetValidInventoryItems();
-
-            for (int i = 0; i < validItems.Length; i++)
-            {
-                purchaseController.TrySellItem(validItems[i]);
-            }
+            playerController.AddCoins(startingCoins);
         }
         #endregion
     }
